cbb76bb4675ba7930aade39d04ae54e4906a7d40
Robert Tabares <robtabares92@gmail.com>
2018-04-24 17:08:20 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,11 @@
 
 def speak_to_grandma(str)
   if str == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO SONNY!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif str == str.upcase
     "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
+
 end
